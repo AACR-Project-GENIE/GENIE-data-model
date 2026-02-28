2efd585ca602396eb1c8725eafeb04ed255d1c17
alexpaynter <alexcpaynter@gmail.com>
2026-02-27 16:08:15 -0800

    Added first derivation function, still need some dependencies

---
+++ R/derive_ca_dx.R
@@ -0,0 +1,55 @@
+#' Derive cancer diagnosis form
+#'
+#' @param raw_ca_dx Cancer diagnosis form from the raw redcap.
+#' @param dat_dict_sub The subset of the data dictionary with variables for this instrument.
+#'
+#' @returns A dataframe similar to ca_dx with additional derivations.
+#' @export
+#'
+#' @examples # Going to need synthetic data probably...
+derive_ca_dx <- function(
+  raw_ca_dx,
+  dat_dict_sub
+) {
+  ca_dx <- column_exclusion_helper_derived(ca_dx)
+
+  rtn <- common_data_derivation_operations(
+    dat = ca_dx,
+    dict = dat_dict_sub
+  )
+
+  rtn <- rtn %>%
+    dplyr::mutate(
+      dob_ca_dx_days = dplyr::case_when(
+        is.na(ca_cadx_int) ~ naaccr_diagnosis_int,
+        T ~ ca_cadx_int
+      )
+    )
+
+  # Add the record level annotations that are jammed into this data for some reason.
+  rtn <- rtn %>%
+    dplyr::group_by(record_id) %>%
+    dplyr::mutate(
+      n_cancers = dplyr::n(),
+      n_cancers_index = sum(redcap_ca_index %in% "Yes")
+    ) %>%
+    dplyr::ungroup(.)
+
+  # rearrange the cancer diagnosis form according to the arcane logic in BPC.
+  rtn <- rtn %>%
+    dplyr::group_by(record_id) %>%
+    # break ties by keeping the order of redcap_ca_seq
+    dplyr::arrange(dob_ca_dx_days, redcap_ca_seq) %>%
+    dplyr::mutate(
+      ca_seq = dplyr::case_when(
+        n_cancers %in% 1 ~ 0,
+        T ~ 1:dplyr::n()
+      )
+    ) %>%
+    dplyr::ungroup(.)
+
+  rtn <- rtn %>%
+    dplyr::relocate(ca_seq, .after = record_id)
+
+  return(rtn)
+}
---
+++ R/z_global_var_dec.R
@@ -0,0 +1,8 @@
+# Used to declare unquoted variables names in dplyr scripts.
+# Most of these are variable names in PRISSMM.
+utils::globalVariables("ca_seq")
+utils::globalVariables("dob_ca_dx_days")
+utils::globalVariables("record_id")
+utils::globalVariables("redcap_ca_index")
+utils::globalVariables("redcap_ca_seq")
+utils::globalVariables(".")
