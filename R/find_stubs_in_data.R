e7bc225a538b26505b5860197e14c0041104b06c
alexpaynter <alexcpaynter@gmail.com>
2026-03-02 22:53:16 -0800

    Stub function desc added

--- R/find_stubs_in_data.R
+++ R/find_stubs_in_data.R
@@ -1,12 +1,20 @@
+#' Find variables with sub names in a list of columns
+#'
+#' @param dat_cols A vector of column names
+#'
+#' @returns A tibble with columns stub (the root), min and max (the highest and lowest appended number after the stub).
+#' @export
+#'
+#' @examples
 find_stubs_in_data <- function(dat_cols) {
-  stub_vars <- tibble(
+  stub_vars <- tibble::tibble(
     stub = dat_cols %>%
       .[str_detect(., '\\_\\_')]
   ) %>%
-    separate(stub, into = c('stub', 'num'), sep = '\\_\\_\\_') %>%
-    mutate(num = as.numeric(num)) %>%
-    group_by(stub) %>%
-    summarize(
+    tidyr::separate(stub, into = c('stub', 'num'), sep = '\\_\\_\\_') %>%
+    dplyr::mutate(num = as.numeric(num)) %>%
+    dplyr::group_by(stub) %>%
+    dplyr::summarize(
       min = min(num),
       max = max(num)
     )
