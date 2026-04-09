f486da9026aba69376c22f482d158d0669cb4b56
alexpaynter <alexcpaynter@gmail.com>
2026-04-09 00:43:10 -0700

    Fixed a bug

--- R/derive_ca_dx.R
+++ R/derive_ca_dx.R
@@ -11,7 +11,7 @@ derive_ca_dx <- function(
   tab,
   dat_dict_sub
 ) {
-  raw_ca_dx <- column_exclusion_helper_derived(raw_ca_dx)
+  raw_ca_dx <- column_exclusion_helper_derived(tab)
 
   rtn <- common_data_derivation_operations(
     dat = raw_ca_dx,
