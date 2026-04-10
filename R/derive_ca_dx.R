cf8b266ca6d9367647f78d63c19f15c18bbba156
alexpaynter <alexcpaynter@gmail.com>
2026-04-09 23:52:41 -0700

    Last one, ready to test out

--- R/derive_ca_dx.R
+++ R/derive_ca_dx.R
@@ -66,7 +66,8 @@ derive_ca_dx <- function(
       ca_clin_t_stage = map_ca_clin_t_stage(ca_clin_t_stage),
       ca_clin_t1_det = map_ca_clin_t1_det(ca_clin_t1_det),
       ca_clin_t2_det = map_ca_clin_t2_det(ca_clin_t2_det),
-      ca_clin_t3_det = map_ca_clin_t3_det(ca_clin_t3_det)
+      ca_clin_t3_det = map_ca_clin_t3_det(ca_clin_t3_det),
+      ca_clin_t4_det = map_ca_clin_t4_det(ca_clin_t4_det)
     )
 
   rtn <- add_mos_yrs_intervals(rtn) # anything ending in days.
--- R/z_global_var_dec.R
+++ R/z_global_var_dec.R
@@ -77,6 +77,7 @@ utils::globalVariables(
     "ca_clin_t_stage",
     "ca_clin_t1_det",
     "ca_clin_t2_det",
-    "ca_clin_t3_det"
+    "ca_clin_t3_det",
+    "ca_clin_t4_det"
   )
 )
