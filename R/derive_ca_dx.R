bcb17ad2718560af73c4b9d4a159d9060255334b
alexpaynter <alexcpaynter@gmail.com>
2026-04-09 23:07:02 -0700

    ca_d_site codes added in from redcap

--- R/derive_ca_dx.R
+++ R/derive_ca_dx.R
@@ -62,7 +62,8 @@ derive_ca_dx <- function(
   rtn <- rtn |>
     dplyr::mutate(
       ca_dx_how = map_ca_dx_how(ca_dx_how),
-      ca_type = map_ca_type(ca_type)
+      ca_type = map_ca_type(ca_type),
+      ca_d_site = map_ca_d_site(ca_d_site)
     )
 
   rtn <- add_mos_yrs_intervals(rtn) # anything ending in days.
