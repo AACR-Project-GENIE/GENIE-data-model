06cbf3be32a3a5bacaed3bb2af95084505185ebb
alexpaynter <alexcpaynter@gmail.com>
2026-04-10 16:21:08 -0700

    Added reg start derivation

--- R/derive_reg.R
+++ R/derive_reg.R
@@ -107,6 +107,8 @@ derive_reg <- function(
 
   rtn <- derive_regimen_drugs(rtn)
 
+  rtn <- derive_reg_start_int(rtn)
+
   # Notes:
   # probably need remove _mask columns too - but not yet.
   return(rtn)
