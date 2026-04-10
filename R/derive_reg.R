db20a3d271565762bb4cfc56128e2159c87f1d70
alexpaynter <alexcpaynter@gmail.com>
2026-04-10 08:45:07 -0700

    Added some functions and docs

---
+++ R/derive_reg.R
@@ -0,0 +1,34 @@
+#' Derive cancer-directed regimen form
+#'
+#' @param tab Cancer-directed drugs (regimen) form from the raw redcap.
+#' @param dat_dict_sub The subset of the data dictionary with variables for this instrument.
+#'
+#' @returns A dataframe similar to regimen with additional derivations.
+#' @export
+#'
+#' @examples # Going to need synthetic data probably...
+derive_reg <- function(
+  tab,
+  dat_dict_sub
+) {
+  tab <- column_exclusion_helper_derived(tab)
+
+  rtn <- common_data_derivation_operations(
+    dat = tab,
+    dict = dat_dict_sub
+  )
+
+  rtn %<>% drugs_ca_rearrangement(., prefix = '^drugs_ca')
+
+  rtn %<>%
+    mutate(
+      across(
+        .cols = matches('^drugs_(start|end|last)dt_int_'),
+        .fns = as.numeric
+      )
+    )
+
+  # Notes:
+  # probably need remove _mask columns too - but not yet.
+  return(rtn)
+}
