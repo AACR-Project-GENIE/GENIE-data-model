18ab5276f699e4a4239521cce6663c65ea775603
alexpaynter <alexcpaynter@gmail.com>
2026-04-12 00:56:15 -0700

    One more for ca_stage_iv

--- R/derive_ca_dx.R
+++ R/derive_ca_dx.R
@@ -78,7 +78,8 @@ derive_ca_dx <- function(
       ca_path_t4_det = map_ca_clin_t4_det(ca_path_t4_det),
       ca_path_n_stage = map_ca_path_n_stage(ca_path_n_stage),
       ca_tx_pre_path_stage = map_ca_tx_pre_path_stage(ca_tx_pre_path_stage),
-      ca_stage = map_ca_stage(ca_stage)
+      ca_stage = map_ca_stage(ca_stage),
+      ca_stage_iv = map_ca_stage_iv(ca_stage_iv)
     )
 
   rtn <- add_mos_yrs_intervals(rtn) # anything ending in days.
