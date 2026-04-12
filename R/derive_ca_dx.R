7e1a738661998dfda13becf0fc02f7edc6d5736f
alexpaynter <alexcpaynter@gmail.com>
2026-04-11 23:17:30 -0700

    Added n stage and path group stage

--- R/derive_ca_dx.R
+++ R/derive_ca_dx.R
@@ -68,7 +68,8 @@ derive_ca_dx <- function(
       ca_clin_t2_det = map_ca_clin_t2_det(ca_clin_t2_det),
       ca_clin_t3_det = map_ca_clin_t3_det(ca_clin_t3_det),
       ca_clin_t4_det = map_ca_clin_t4_det(ca_clin_t4_det),
-      ca_clin_n_stage = map_ca_clin_n_stage(ca_clin_n_stage)
+      ca_clin_n_stage = map_ca_clin_n_stage(ca_clin_n_stage),
+      ca_path_group_stage = map_ca_path_group_stage(ca_path_group_stage)
     )
 
   rtn <- add_mos_yrs_intervals(rtn) # anything ending in days.
--- R/z_global_var_dec.R
+++ R/z_global_var_dec.R
@@ -110,6 +110,7 @@ utils::globalVariables(
     "drugs_num",
     "drugs_inst",
     "drugs_firstinst",
-    "ca_clin_n_stage"
+    "ca_clin_n_stage",
+    "ca_path_group_stage"
   )
 )
