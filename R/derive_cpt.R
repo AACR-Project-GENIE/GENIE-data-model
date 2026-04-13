71a838557bd301d142f1ddfb8ae67f041e0e382c
alexpaynter <alexcpaynter@gmail.com>
2026-04-12 23:34:45 -0700

    Added derive_cpt()

---
+++ R/derive_cpt.R
@@ -0,0 +1,31 @@
+#' Derive CPT (cancer panel test) form
+#'
+#' @param tab Cancer panel test form from the raw redcap.
+#' @param dat_dict_sub The subset of the data dictionary with variables for
+#'   this instrument.
+#'
+#' @returns A dataframe similar to cpt with additional derivations.
+#' @export
+#'
+#' @examples
+#' # derive_cpt(cpt, dat_dict_sub)
+derive_cpt <- function(
+  tab,
+  dat_dict_sub
+) {
+  tab <- column_exclusion_helper_derived(tab)
+
+  cpt_ca_cols <- dat_dict_sub |>
+    dplyr::filter(stringr::str_detect(field_name, "^cpt_ca___")) |>
+    dplyr::pull(field_name)
+
+  rtn <- common_data_derivation_operations(
+    dat = tab,
+    dict = dat_dict_sub,
+    exclude_cols = cpt_ca_cols
+  )
+
+  rtn <- drugs_ca_rearrangement(rtn, prefix = "^cpt_ca")
+
+  return(rtn)
+}
