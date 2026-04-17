7577011c4a1ef531c22f9d11ec352685cb380f28
alexpaynter <alexcpaynter@gmail.com>
2026-04-16 23:53:59 -0700

    Added scan_sites var

--- R/build_dv_tab_img.R
+++ R/build_dv_tab_img.R
@@ -26,6 +26,8 @@ build_dv_tab_img <- function(
 
   rtn <- repair_scan_sites(rtn)
 
+  rtn <- derive_scan_sites(rtn)
+
   rtn <- rtn |>
     dplyr::mutate(
       image_inst_perf = map_image_inst(image_inst_perf),
