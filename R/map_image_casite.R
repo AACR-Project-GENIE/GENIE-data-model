c3df0c881871e084304c86b414db4c9169cfd2b9
alexpaynter <alexcpaynter@gmail.com>
2026-04-17 00:23:49 -0700

    Reverted back - not working and I don't really care to match whatever insane whitespace convention they had

--- R/map_image_casite.R
+++ R/map_image_casite.R
@@ -4,18 +4,14 @@
 #' `image_casite` variables.
 #'
 #' @param x A numeric (or character) vector of site codes.
-#' @param whitespace_match_legacy If `TRUE`, use four spaces between the ICD
-#'   code and description to match the legacy data format. Default `FALSE`.
 #'
 #' @returns A character vector of labels.
 #' @export
 #'
 #' @examples
 #' map_image_casite(c(349, 809))
-map_image_casite <- function(x, whitespace_match_legacy = TRUE) {
+map_image_casite <- function(x) {
   labels <- load_met_site_map()
-  if (whitespace_match_legacy) {
-    labels <- sub(" ", "    ", labels)
-  }
+
   dplyr::recode(as.character(x), !!!labels)
 }
